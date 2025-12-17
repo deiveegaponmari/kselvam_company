83845a0f26e8f93bf8ea0844de231a28171da6fa
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-12-17 13:03:08 +0530

    changes added

--- src/components/EventsHome.jsx
+++ src/components/EventsHome.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import api from '../api/apiConfig'
 function EventsHome() {
   const navigate = useNavigate();
    const [event, setEvent] = useState([]);
